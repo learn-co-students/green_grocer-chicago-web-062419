381b925d8bc2cd1e03ff5fc2b55c1e80c9a0ecad
Amanda Treutler <amandathedev@gmail.com>
2019-06-16 19:04:04 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,61 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  cart.each do |item|
+    item.each do |property, price|
+      if cart_hash[property].nil?
+        cart_hash[property] = price.merge({:count => 1})
+      else
+        cart_hash[property][:count] += 1
+      end
+    end
+  end
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  discounted = {}
+  cart.each do |item, info|
+    coupons.each do |coupon|
+      if item == coupon[:item] && info[:count] >= coupon[:num]
+        cart[item][:count] = cart[item][:count] - coupon[:num]
+        if discounted[item + " W/COUPON"]
+          discounted[item + " W/COUPON"][:count] += 1
+        else
+          discounted[item + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item][:clearance], :count => 1}
+        end
+      end
+    end
+    discounted[item] = info
+
+  end
+  discounted
 end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item, discount|
+    if discount[:clearance] == true
+      discount[:price] = (discount[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  
+  total = 0
+  
+  cart2.each do |name, price|
+    total += price[:price] * price[:count]
+  end
+  
+  total > 100 ? total * 0.9 : total
+  
 end
+
